d9bf2681fa9aee05886cf9a9e6229130ce94b37a
Michael Moscatt <michael.moscatt@gmail.com>
2020-06-14 18:00:17 -0400

    Added dynamic port value for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 var path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 // Socket
 var http = require('http').createServer(app);
